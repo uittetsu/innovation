067795afefdfaa21f133e2d248389fd72b28703d
内山一哲 <>
2024-05-07 15:55:22 +0900

    commit

--- projects/GaussianExample-HDRP/Assets/scripts/GateController.cs
+++ projects/GaussianExample-HDRP/Assets/scripts/GateController.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class GateController : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     SceneManager.LoadScene("NU");
+    // }
+
+    // void OnCollisionEnter(Collision collisionInfo)
+    // {
+    //     SceneManager.LoadScene("NU_syokudomae");
+    // }
+
+    void OnCollisionStay(Collision collisionInfo)
     {
-        SceneManager.LoadScene("NU");
+        if (Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene("NU_syokudomae");
+        }
     }
 }
