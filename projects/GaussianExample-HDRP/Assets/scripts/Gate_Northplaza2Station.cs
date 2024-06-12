0a167bb4da4f393fdead16dc2029c3a0d3e5b49d
内山一哲 <>
2024-06-13 08:32:41 +0900

    commit

--- projects/GaussianExample-HDRP/Assets/scripts/Gate_Northplaza2Station.cs
+++ projects/GaussianExample-HDRP/Assets/scripts/Gate_Northplaza2Station.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Gate_shokudomae3EIkanmae : MonoBehaviour
+public class Gate_Northplaza2Station : MonoBehaviour
 {
     void OnCollisionStay(Collision collisionInfo)
     {
         if (Input.GetKey(KeyCode.Return))
         {
-            SceneManager.LoadScene("NU_EIkanmae");
+            SceneManager.LoadScene("Station");
         }
     }
 }
